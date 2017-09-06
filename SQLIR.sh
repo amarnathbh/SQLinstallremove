334425c4370369ac9f6ee4eb9d1bfcc8e7a258d7
Amarnath B H <abg55@mail.umkc.edu>
2017-09-05 23:16:31 -0400

    Update SQLIR.sh

--- SQLIR.sh
+++ SQLIR.sh
@@ -1,15 +1,29 @@
 
 #!/bin/bash
 apt-get  update
+
+# install sql server
 apt-get install mysql-server -y
+
+#install sql client
+apt-get install mysql-client -y
+
+#start mysql
 service  mysql  start
+
 echo "Mysql server started"
 #mysql --user root  -proot
 echo "logged out of SQL"
 #service mysql status
+
+#stop sql server
+
 service mysql stop
 echo "mysql server stopped"
+
+#remove mysql 
+
 apt-get  --purge remove mysql* -y
 apt-get autoremove -y
 apt-get autoclean
-#echo "mysql server removed bye bye!"
+echo "mysql server removed bye bye!"
